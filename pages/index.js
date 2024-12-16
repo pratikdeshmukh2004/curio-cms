6c58dde7beb2a0050e33ddcc82a06169a4ac216a
Pratik Deshmukh <pratikdeshmukhlobhi@gmail.com>
2024-12-16 23:02:01 +0530

    google auth provider

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import Link from "next/link";
+import {
+  GoogleOAuthProvider,
+  useGoogleLogin,
+  useGoogleOneTapLogin,
+} from "@react-oauth/google";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      setUser(user);
+    }
+  }, []);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between lg:p-24 p-10 ${inter.className}`}
@@ -33,6 +48,7 @@ body {
 }
 `}
         </style>
+        <title>CurIO</title>
       </Head>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p></p>
@@ -56,13 +72,20 @@ body {
       </div>
 
       <div className="mb-52 flex lg:flex-row flex-col justify-center gap-5 text-center w-full lg:max-w-xl lg:w-full lg:mb-0  lg:text-left">
-        <Link
-          href={"/diagrams"}
-          className="border-2 border-gray-500 text-gray-300 p-5 rounded-xl font-bold hover:border-sky-700 hover:text-sky-700 cursor-pointer"
-        >
-          View Diagrams
-          <FontAwesomeIcon className="ml-2 text-xs" icon={faDiagramProject} />
-        </Link>
+        {user ? (
+          <Link
+            href={"/diagrams"}
+            className="border-2 border-gray-500 text-gray-300 p-5 rounded-xl font-bold hover:border-sky-700 hover:text-sky-700 cursor-pointer"
+          >
+            View Diagrams
+            <FontAwesomeIcon className="ml-2 text-xs" icon={faDiagramProject} />
+          </Link>
+        ) : (
+          <GoogleOAuthProvider clientId="165709508753-3fmsce8o2knrfccucibes57h9c72vr8g.apps.googleusercontent.com">
+            <GoogleLogin />
+          </GoogleOAuthProvider>
+        )}
+
         {/* <Link
           href={"/pointers"}
           className="border-2 border-gray-500 text-gray-300 p-5 rounded-xl font-bold hover:border-sky-700 hover:text-sky-700 cursor-pointer"
@@ -141,3 +164,66 @@ body {
     </main>
   );
 }
+
+const GoogleLogin = () => {
+  const handleSuccess = (credentialResponse) => {
+    const token = credentialResponse.access_token;
+    fetch("https://www.googleapis.com/oauth2/v3/userinfo", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((userInfo) => {
+        localStorage.setItem("user", JSON.stringify(userInfo));
+        window.location.href = "/diagrams";
+      })
+      .catch((error) => {
+        console.error("Failed to fetch user info: ", error);
+      });
+  };
+
+  const login = useGoogleLogin({
+    onSuccess: handleSuccess,
+  });
+
+  useGoogleOneTapLogin({
+    onSuccess: handleSuccess,
+    onError: () => {
+      console.log("Login Failed");
+    },
+  });
+  return (
+    <button
+      className="border flex items-center gap-2 border-gray-500 text-gray-300 p-2 px-5 rounded-full font-bold hover:border-sky-700 hover:text-sky-700 cursor-pointer"
+      onClick={login}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        width="35"
+        height="35"
+        viewBox="0 0 48 48"
+      >
+        <path
+          fill="#FFC107"
+          d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+        ></path>
+        <path
+          fill="#FF3D00"
+          d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+        ></path>
+        <path
+          fill="#4CAF50"
+          d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+        ></path>
+        <path
+          fill="#1976D2"
+          d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+        ></path>
+      </svg>
+      Google Login
+    </button>
+  );
+};
